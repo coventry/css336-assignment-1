ecfecd6c74a848eac363a1b9764b24ae16aaaf46
Alex Coventry <alx@mit.edu>
2026-08-09 21:18:00 +0000

    Add more common file paths

--- cs336_basics/common.py
+++ cs336_basics/common.py
@@ -6,7 +6,17 @@ ROOT_PATH = pathlib.Path("/workspace/repo")
 if (spath := str(ROOT_PATH)) not in sys.path:
     sys.path.append(spath)
 
-TRAIN_PATH = ROOT_PATH / "data/TinyStoriesV2-GPT4-train.txt"
-VALIDATION_PATH = ROOT_PATH / "data/TinyStoriesV2-GPT4-valid.txt"
+DATA_PATH = ROOT_PATH / "data"
+
+TRAIN_PATH = DATA_PATH / "TinyStoriesV2-GPT4-train.txt"
+VALIDATION_PATH = DATA_PATH / "TinyStoriesV2-GPT4-valid.txt"
+TOKENIZER_VOCAB_PATH = DATA_PATH / "vocab.txt"
+TOKENIZER_MERGES_PATH = DATA_PATH / "merges.txt"
+
+OWT_TRAIN_PATH = DATA_PATH / "owt_train.txt"
+OWT_VALIDATION_PATH = DATA_PATH / "owt_valid.txt"
+OWT_TOKENIZER_VOCAB_PATH = DATA_PATH / "owt-vocab.txt"
+OWT_TOKENIZER_MERGES_PATH = DATA_PATH / "owt-merges.txt"
+
 
 EOT_TOKEN = "<|endoftext|>"
