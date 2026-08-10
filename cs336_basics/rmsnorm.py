24cecaa9ce427b8a33ed71fb086c5e5a445df79c
Alex Coventry <alx@mit.edu>
2026-08-10 01:15:50 +0000

    Solution to `rmsnorm` problem from the assignment

---
+++ cs336_basics/rmsnorm.py
@@ -0,0 +1,44 @@
+"""Implementation of RMSNorm NN module
+
+Response to problem `rmsnorm` of Assignment 1."""
+
+import einx
+
+import torch
+from torch import nn, Tensor
+from jaxtyping import Float
+
+
+class RMSNorm(nn.Module):
+
+    def __init__(
+        self,
+        d_model: int,
+        eps: float = 1e-5,
+        device: torch.device | None = None,
+        dtype: torch.dtype | None = None,
+    ):
+        super().__init__()
+        self.d_model = d_model
+        self.eps = eps
+        gain = torch.ones(d_model, device=device, dtype=dtype)
+        self.gain = nn.Parameter(gain)
+
+    def forward(
+        self, x: Float[Tensor, "*batch d_model"]
+    ) -> Float[Tensor, "*batch d_model"]:
+        in_dtype = x.dtype
+        x = x.to(torch.float32)  # Upcast to avoid oveflow from squaring.
+        length_sq = einx.dot(  # pyright: ignore[reportPrivateImportUsage]
+            "... d_model, ... d_model -> ...", x, x, d_model=self.d_model
+        )
+        rms = torch.sqrt(length_sq / self.d_model + self.eps)
+        gained = einx.multiply(  # pyright: ignore[reportPrivateImportUsage]
+            "... d_model, d_model -> ... d_model",
+            x,
+            self.gain,
+            d_model=self.d_model,
+        )
+        return einx.divide(  # pyright: ignore[reportPrivateImportUsage]
+            "... d_model, ... -> ... d_model", gained, rms, d_model=self.d_model
+        )
--- tests/adapters.py
+++ tests/adapters.py
@@ -13,6 +13,7 @@ from cs336_basics.train_tokenizer import train_bpe
 from cs336_basics.tokenizer import Tokenizer
 from cs336_basics.linear import Linear
 from cs336_basics.embedding import Embedding
+from cs336_basics.rmsnorm import RMSNorm
 
 
 def run_linear(
@@ -385,7 +386,9 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+    r = RMSNorm(d_model, eps, device=weights.device, dtype=weights.dtype)
+    r.load_state_dict({"gain": weights})
+    return r(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
