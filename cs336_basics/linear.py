Fix variance scale for stacked Q/K/V initializations

cs336_basics/linear.py
@@ -20,6 +20,7 @@ class Linear(nn.Module):
     Args:
         in_features: Expected size of input vectors
         out_features: Size of output vectors
+        init_variance_scale: Optionally change σ² by this factor
 
     Both arguments must be positive.
     """
@@ -28,6 +29,7 @@ class Linear(nn.Module):
         self,
         in_features: int,
         out_features: int,
+        init_variance_scale: float = 1,
         device: torch.device | None = None,
         dtype: torch.dtype | None = None,
     ):
@@ -37,9 +39,14 @@ class Linear(nn.Module):
         if out_features < 1:
             err = f"out_features must be positive, got {out_features}"
             raise ValueError(err)
+        if init_variance_scale <= 0:
+            raise ValueError(
+                "init_variance_scale must be positive, got "
+                f"{init_variance_scale}"
+            )
         self.in_features = in_features
         self.out_features = out_features
-        sigma = sqrt(2 / (in_features + out_features))
+        sigma = sqrt(2 * init_variance_scale / (in_features + out_features))
         bound = 3 * sigma
         init_weight = torch.empty(
             out_features, in_features, device=device, dtype=dtype

cs336_basics/multihead_self_attention.py
@@ -44,9 +44,21 @@ class MultiHeadSelfAttention(nn.Module):
         self.d_model = d_model
         self.num_heads = num_heads
         self.d_k = d_model // num_heads
-        # Hidden-dim -> vertically stacked W_Q, W_K, W_V projections
+        # Hidden-dim -> vertically stacked W_Q, W_K, W_V projections.
+        # Assignment suggests this arrangement so that only a single matmul
+        # occurs.
         self.attn_projections = Linear(
-            d_model, 3 * d_model, device=device, dtype=dtype
+            d_model,
+            3 * d_model,
+            device=device,
+            dtype=dtype,
+            # Linear's initialization σ² assumes it's creating a single linear
+            # function, but here we are abusing it to create three stacked
+            # linear functions. Its default σ² for a (d_model, 3*d_model)
+            # function is 1/(2*d_model) (see §3.3.1), but the required variance
+            # for the actual (d_model, d_model) components is 1/d_model.
+            # Therefore, we multiply the σ² initialization by 2 to compensate.
+            init_variance_scale=2,
         )
         self.out_projection = Linear(
             d_model, d_model, device=device, dtype=dtype