6c71698955379f2e201cfe9965af1c48a77e8418
Alex Coventry <alx@mit.edu>
2026-08-14 03:13:22 +0000

    Fix softmax docstring

--- cs336_basics/softmax.py
+++ cs336_basics/softmax.py
@@ -11,7 +11,7 @@ from jaxtyping import Float
 def softmax(
     in_features: Float[Tensor, "..."], dim: int
 ) -> Float[Tensor, "..."]:
-    "Return softmax value from eq. (10), over last dimension of in_features"
+    "Return softmax value from eq. (10), over dimension dim of in_features"
     # keepdim=True to broadcast the max-contracted dimension over in_features
     normalized = in_features - in_features.amax(dim=dim, keepdim=True)
     numerators = normalized.exp()
